b10a20af835adbef8830b4f5c96ea72bfc297f6a
Manju Vasanth <74921345+ManjuVasanth@users.noreply.github.com>
2025-01-29 21:42:10 -0600

    Add files via upload
    initial commit

---
+++ departmentservice/src/test/java/com/manju/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manju.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ employee-service/src/main/java/com/manju/employee_service/repository/EmployeeRepository.java
@@ -0,0 +1,39 @@
+package com.manju.employee_service.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.manju.employee_service.model.Employee;
+
+@Repository
+public class EmployeeRepository {
+
+	private List<Employee> employees = new ArrayList<>();
+	
+	public Employee add(Employee employee) {
+		employees.add(employee);
+		return employee;
+		
+	}
+	
+	public Employee findEmployeeById(Long id) {
+		return employees.stream().filter(a->a.id().equals(id))
+				.findFirst().orElseThrow();
+		
+	}
+	
+	public List<Employee> findAll() {
+		return employees;
+		
+	}
+	
+	public List<Employee> findByDepartment(Long departmentId) {
+		return employees.stream()
+				.filter(a->a.departmentId().equals(departmentId))
+				.toList();
+		
+	}
+	
+}
